923ec5647769f987228785ee105e8adc764ce429
locit2003 <locit2003@gmail.com>
2023-03-31 00:41:03 +0700

    clean code

--- locShopApi/src/main/java/com/locShop/LocShopApiApplication.java
+++ locShopApi/src/main/java/com/locShop/LocShopApiApplication.java
@@ -13,7 +13,6 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@ComponentScan("com.locShop")
 @OpenAPIDefinition(
 		info = @Info(title = "books API",version = "2.0.4"),
 		servers = {@Server(url = "http://localhost:8088"),@Server(url = "http://oneone.com")},
@@ -23,23 +22,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SecurityScheme(name = "BearerJWT", type = SecuritySchemeType.HTTP,scheme = "bearer", bearerFormat = "JWT",
 description = "Brearer token for project books store")
 public class LocShopApiApplication implements WebMvcConfigurer{
-
-
-//	private JwtInterceptor tokenInterceptor;
-//
-//	public LocShopApiApplication(JwtInterceptor tokenInterceptor) {
-//		this.tokenInterceptor = tokenInterceptor;
-//	}
-//
-//	public void addInterceptors(InterceptorRegistry registry) {
-//		registry.addInterceptor(tokenInterceptor);
-//	}
-//
-//	public void configure(WebSecurity web)  {
-//		web.ignoring().requestMatchers("/v3/api-docs",
-//				"/swagger-ui.html",
-//				"/swagger-ui/**");
-//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LocShopApiApplication.class, args);
