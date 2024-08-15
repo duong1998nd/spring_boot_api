9c1437f777f992c3a96c8143a1418c5b8fc79553
duong1998nd <nguyenquangduong@MacBook-Pro-cua-Nguyen.local>
2024-08-15 14:11:13 +0700

    2nd commit

---
+++ bookShopApi/src/test/java/com/bShop/BShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocShopApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
